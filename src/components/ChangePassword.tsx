01bb52104ccbe275d37ec748d7bec08e7f0a032e
Vaidotas Gedzius <vaidotas@gedzius.lt>
2018-09-26 21:30:09 +0100

    My details and changes password forms added

--- src/components/ChangePassword.tsx
+++ src/components/ChangePassword.tsx
@@ -3,13 +3,22 @@ import {
   StyleRulesCallback, Theme,
   withStyles, WithStyles
 } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-type ComponentClassNames = 'root';
+type ComponentClassNames =
+  | 'root'
+  | 'margin'
 
 export const styles: StyleRulesCallback = (theme: Theme) => ({
   root: {
-    padding: theme.spacing.unit * 3
+    padding: theme.spacing.unit * 3,
+    width: '50%',
   },
+  margin: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  }
 });
 
 class ChangePassword extends React.Component<WithStyles<ComponentClassNames>> {
@@ -17,7 +26,12 @@ class ChangePassword extends React.Component<WithStyles<ComponentClassNames>> {
     const { classes } = this.props;
 
     return (
-      <div className={classes.root}>Change Password</div>
+      <div className={classes.root}>
+        <TextField fullWidth className={classes.margin} label="Current Password" />
+        <TextField fullWidth className={classes.margin} label="New Password" />
+        <TextField fullWidth className={classes.margin} label="Confirm" />
+        <Button className={classes.margin} variant="raised" color="primary">Change</Button>
+      </div>
     );
   }
 }
--- src/components/MyDetails.tsx
+++ src/components/MyDetails.tsx
@@ -3,23 +3,66 @@ import {
   StyleRulesCallback, Theme,
   withStyles, WithStyles
 } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-type ComponentClassNames = 'root';
+import { AuthContext } from '../contexts/AuthContext';
+import { User } from '../servises/Users';
+
+type ComponentClassNames =
+  | 'root'
+  | 'margin';
+
+interface MyDetailsProps {
+  user: User;
+}
 
 export const styles: StyleRulesCallback = (theme: Theme) => ({
   root: {
-    padding: theme.spacing.unit * 3
+    padding: theme.spacing.unit * 3,
+    width: '50%',
   },
+  margin: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  }
 });
 
-class MyDetails extends React.Component<WithStyles<ComponentClassNames>> {
+class MyDetails extends React.Component<MyDetailsProps & WithStyles<ComponentClassNames>> {
+  public state = {
+    firstName: '',
+    lastName: '',
+    email: ''
+  }
+
+  public componentWillMount() {
+    this.setState(this.props.user);
+  }
+
   public render() {
-    const { classes } = this.props;
+    const { classes, } = this.props;
+    const { firstName, lastName, email } = this.state;
 
     return (
-      <div className={classes.root}>My Details</div>
+      <div className={classes.root}>
+        <TextField fullWidth className={classes.margin} label="First Name" value={firstName} />
+        <TextField fullWidth className={classes.margin} label="Last Name" value={lastName} />
+        <TextField fullWidth className={classes.margin} label="Email" value={email} />
+        <Button className={classes.margin} variant="raised" color="primary">Update</Button>
+      </div>
     );
   }
 }
 
-export default withStyles(styles)(MyDetails);
+const StyledMyDetails = withStyles(styles)(MyDetails);
+
+export default (props: any) => (
+  <AuthContext.Consumer>
+    {(auth) => (
+      <StyledMyDetails
+        {...props}
+        user={auth.user}
+      />
+    )}
+  </AuthContext.Consumer>
+);
