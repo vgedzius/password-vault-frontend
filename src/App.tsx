93b9dceec44718010b04916c92ed80e5c8d94ef8
Vaidotas Gedzius <vaidotas@gedzius.lt>
2018-09-30 13:00:59 +0100

    Theming support

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom'
 
 import Main from './components/Main';
 import AuthProvider from './contexts/AuthContext';
 
+const theme = createMuiTheme({
+  
+});
+
 class App extends React.Component {
   public render() {
     return (
-      <BrowserRouter>
-        <AuthProvider loadingComponent={<div>Loading...</div>}>
-          <Main />
-        </AuthProvider>
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <AuthProvider loadingComponent={<div>Loading...</div>}>
+            <Main />
+          </AuthProvider>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
