ca66981aad6b13fc05948311c40b5a0609d892b7
Vaidotas Gedzius <vaidotas@gedzius.lt>
2018-10-01 20:55:01 +0100

    Use icon button

--- src/components/UserRow.tsx
+++ src/components/UserRow.tsx
@@ -4,31 +4,24 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { User } from '../servises/Users';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 export interface UserRowProps {
   user: User;
 }
 
 export default class UserRow extends React.Component<UserRowProps> {
-  public state = {
-    hover: false,
-  }
-
   public render() {
     const { user } = this.props;
-    const { hover } = this.state;
 
     return (
       <TableRow hover onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
         <TableCell>{user.firstName} {user.lastName}</TableCell>
         <TableCell>{user.email}</TableCell>
         <TableCell>
-          {hover && (
-            <Button mini variant="fab" color="secondary">
-              <DeleteIcon />
-            </Button>
-          )}
+          <IconButton color="secondary">
+            <DeleteIcon fontSize="small" />
+          </IconButton >
         </TableCell>
       </TableRow>
     )
